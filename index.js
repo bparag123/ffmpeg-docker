ee76cc76065f72f56b3efdc88098a7acbae70b3e
Parag Bharadva <parag.b@simformsolutions.com>
2022-07-25 11:06:22 +0530

    Bug fix for cors

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ import express from 'express'
 import { getRecording } from './mergeVideos.js'
 const app = express()
 
-app.get('/:meetingId',async (req, res) => {
+app.get('/:meetingId', async (req, res) => {
     const { meetingId } = req.params
-    const result = await getRecording(meetingId)
-    res.send(result)
+    const { headers, statusCode, body } = await getRecording(meetingId)
+    res.set(headers)
+    res.status(statusCode).send({
+        body
+    })
 })
 
 app.listen(3000, () => {
